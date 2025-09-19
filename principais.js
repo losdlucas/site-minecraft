26979c86cc15056ca3da075095f69cf425f53230
losdlucas <lucas.o.silva45@aluno.senai.br>
2025-09-19 10:25:51 -0300

    Add files via upload

---
+++ principais.js
@@ -0,0 +1,9 @@
+function mostrarMensagem(todos) {
+    if (todos == 'Madeira') {
+        window.location.href = 'madeira.html';
+    } else if (todos == 'Pedra') {
+        window.location.href = 'pedra.html';
+    } else if (todos == 'Ferro') {
+        window.location.href = 'ferro.html';
+    }    
+}
